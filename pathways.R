f1db180cd167cf68dc35ce425d27e8aaae69e68f
Maria Chavez <materechm@gmail.com>
2014-12-07 14:05:45 -0600

    Update pathways.R

--- pathways.R
+++ pathways.R
@@ -2,9 +2,12 @@ library(org.Hs.eg.db)
 library(KEGGgraph)
 library(Rgraphviz)
 
-var1 <- system.file("extdata/hsa00020.xml",package="KEGGgraph")
+retrieveKGML(pathwayid='05014', organism='hsa', destfile='hsa05014')
+retrieveKGML(pathwayid='05010', organism='hsa', destfile='hsa05010')
+
+var1 <- system.file("extdata/hsa05014.xml",package="KEGGgraph",  method="wget")
 var2 <- parseKGML2Graph(var1, genesOnly=TRUE)
-var3 <- c("hsa:4967",edges(var2)$'hsa:55753')
+var3 <- c("hsa:836",edges(var2)$'hsa:836')
 var4 <- subKEGGgraph(var3,var2)
 var5 <- sapply(edges(var4), length) > 0
 var6 <- sapply(inEdges(var4), length) > 0
@@ -13,12 +16,18 @@ var8 <- translateKEGGID2GeneID(names(var7))
 var9 <- sapply(mget(var8,org.Hs.egSYMBOL),"[[",1)
 var10 <- var4
 nodes(var10) <- var9
-#var11 <- list();
-#var11$fillcolor <- makeAttr(var4,"col=635")
-plot(var10) #, nodeAttrs=var11)
-retrieveKGML(pathwayid='05014', organism='hsa', destfile='hsa05014')
-var12 <- system.file("extdata/hsa00020.xml",package="KEGGgraph")
-var13 <- parseKGML2DataFrame(var12, reactions=FALSE)
-var14 <- system.file("extdata/hsa00020.xml",package="KEGGgraph")
-var15 <- parseKGML2DataFrame(var14, reactions=FALSE)
-var16 <- intersect(var13, var15)
+
+var12 <- system.file("extdata/hsa05010.xml",package="KEGGgraph",  method="wget")
+var13 <- parseKGML2Graph(var12, genesOnly=TRUE)
+var14 <- c("hsa:836",edges(var13)$'hsa:836')
+var15 <- subKEGGgraph(var14,var13)
+var16 <- sapply(edges(var15), length) > 0
+var17 <- sapply(inEdges(var15), length) > 0
+var18 <- var16|var17
+var19 <- translateKEGGID2GeneID(names(var18))
+var20 <- sapply(mget(var19,org.Hs.egSYMBOL),"[[",1)
+var21 <- var15
+nodes(var21) <- var20
+
+var22 <- intersect(var20, var9)
+plot(var22)
